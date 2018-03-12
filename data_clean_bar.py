a566b8339ce8cb28e4b38b6152d511e616fa177f
Shoujun <shoujun.gu@gmail.com>
2018-03-12 01:02:57 -0400

    change

--- data_clean_bar.py
+++ data_clean_bar.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 df=pd.read_csv('data.csv')
-df=df.loc[:,['abbv','date_cer','ncs','pts','sps','hhas','sq','sqr']]
+df=df.loc[:,['abbv','date_cer','ncs','pts','sps','hhas','c_sqr']]
 
 
 
